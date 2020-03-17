e6e77059a059c52d214775c3981cde76602aecf0
Cole Davis <colebuildanddevelop@gmail.com>
2020-03-17 19:26:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,10 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  # Your code here
+  array.map { |ele| 
+
 end
+
 
 def long_planeteer_calls# code an argument here
   # Your code here
